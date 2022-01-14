8c9a228a29413ad1ecf74af3431b0343a6f22e97
Nicolas Zanotti <nicolaszanotti@me.com>
2022-01-14 11:32:43 +0100

    fix: use production websocket endpoint 🙄

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("wss://co2-calculator-api.herokuapp.com/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')

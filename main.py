b78fc1390a566517170247ffc6abfd6d3b067964
Ben Krikler <4083697+benkrikler@users.noreply.github.com>
2022-01-14 15:57:32 +0100

    Add /online end point

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from typing import Optional
 from fastapi import FastAPI
+from vc_calculator.interface import make_device, OnlineDetails, compute
+import vc_calculator.interface as online
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
+
+
+@app.post("/online")
+def calculate(body: online.OnlineDetails):
+    devices = body.device_list
+    devices = [online.make_device(d) for d in devices]
+    results = online.compute(devices, body.bandwidth)
+    return results

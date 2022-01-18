05e240ac28566a4567e990c5c96d5e3f839e4f84
Freddie Ruxton <ruxton.f@gmail.com>
2022-01-18 15:09:13 +0000

    Move Modules to module_interface.py and rename to ModuleInterfaces

--- app/api/module_interface.py
+++ app/api/module_interface.py
@@ -1,6 +1,7 @@
+import itertools
 from typing import Any, Type, Generic, TypeVar, Callable
 
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 from pydantic import BaseModel
 
 RequestT = TypeVar("RequestT", bound=BaseModel)
@@ -41,3 +42,37 @@ class ModuleInterface(Generic[RequestT, ResponseT]):
     @property
     def interface(self: "ModuleInterface") -> tuple[dict[str, Any], dict[str, Any]]:
         return self.request_schema, self.response_schema
+
+
+class ModuleInterfaces:
+    def __init__(
+        self: "ModuleInterfaces", module_interfaces: list[ModuleInterface]
+    ) -> None:
+        self._modules = module_interfaces
+        self._modules_by_name = {m.name: m for m in module_interfaces}
+
+    @property
+    def modules(self: "ModuleInterfaces") -> list[ModuleInterface]:
+        return self._modules
+
+    @property
+    def modules_by_name(self: "ModuleInterfaces") -> dict[str, ModuleInterface]:
+        return self._modules_by_name
+
+    @property
+    def interfaces(self: "ModuleInterfaces") -> list[dict[str, Any]]:
+        interfaces = [module.interface for module in self.modules]
+        interfaces = list(itertools.chain.from_iterable(interfaces))
+        return interfaces
+
+    @property
+    def request_schemas(self: "ModuleInterfaces") -> list[dict[str, Any]]:
+        return [module.request_schema for module in self.modules]
+
+    @property
+    def response_schemas(self: "ModuleInterfaces") -> list[dict[str, Any]]:
+        return [module.response_schema for module in self.modules]
+
+    def include_routers(self: "ModuleInterfaces", app: FastAPI) -> None:
+        for module in self.modules:
+            app.include_router(module.router)
--- app/api/modules.py
+++ app/api/modules.py
@@ -1,45 +1,8 @@
-import itertools
-from typing import Any
-
-from fastapi import FastAPI
-
 from app.api import another_module, flight_calculator, template_module, vc_calculator
-from app.api.module_interface import ModuleInterface
+from app.api.module_interface import ModuleInterfaces
 
 
-class Modules:
-    def __init__(self: "Modules", module_interfaces: list[ModuleInterface]) -> None:
-        self._modules = module_interfaces
-        self._modules_by_name = {m.name: m for m in module_interfaces}
-
-    @property
-    def modules(self: "Modules") -> list[ModuleInterface]:
-        return self._modules
-
-    @property
-    def modules_by_name(self: "Modules") -> dict[str, ModuleInterface]:
-        return self._modules_by_name
-
-    @property
-    def interfaces(self: "Modules") -> list[dict[str, Any]]:
-        interfaces = [module.interface for module in self.modules]
-        interfaces = list(itertools.chain.from_iterable(interfaces))
-        return interfaces
-
-    @property
-    def request_schemas(self: "Modules") -> list[dict[str, Any]]:
-        return [module.request_schema for module in self.modules]
-
-    @property
-    def response_schemas(self: "Modules") -> list[dict[str, Any]]:
-        return [module.response_schema for module in self.modules]
-
-    def include_routers(self: "Modules", app: FastAPI) -> None:
-        for module in self.modules:
-            app.include_router(module.router)
-
-
-modules = Modules(
+modules = ModuleInterfaces(
     [
         another_module.module,
         flight_calculator.module,

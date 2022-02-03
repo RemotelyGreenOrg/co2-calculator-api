950a782cc878924a352e5a2e3185083a2807ba61
Freddie Ruxton <ruxton.f@gmail.com>
2022-02-03 19:10:23 +0000

    Fix prestart.sh filepaths

--- prestart.sh
+++ prestart.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # Let the DB start
-python /app/prestart.py
+python ./app/prestart.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python /app/initial_data.py
+python ./app/initial_data.py

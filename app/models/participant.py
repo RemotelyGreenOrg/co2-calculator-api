53e0759768a774678ff894ef807a44b900602b75
Freddie Ruxton <ruxton.f@gmail.com>
2022-02-04 19:09:45 +0000

    Add Participant model

---
+++ app/models/participant.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, Integer, Float, Boolean, Enum, ForeignKey
+
+from app.db.base_class import Base
+from app.schemas.common import JoinMode
+
+if TYPE_CHECKING:
+    from .event import Event  # noqa
+
+
+class Participant(Base):
+    __tablename__ = "participants"
+
+    id = Column(Integer, primary_key=True, index=True)
+    join_mode = Column(Enum(JoinMode))
+    lon = Column(Float)
+    lat = Column(Float)
+    active = Column(Boolean)
+    event_id = Column(Integer, ForeignKey('event.id'))

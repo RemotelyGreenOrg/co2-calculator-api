b19af6e65e74a730e0f64016dcd9cb357b7962c6
Freddie Ruxton <ruxton.f@gmail.com>
2022-02-04 19:10:41 +0000

    Add Participant schemas

---
+++ app/schemas/participant.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from app.schemas.common import JoinMode
+
+
+# Shared properties
+class ParticipantBase(BaseModel):
+    join_mode: Optional[JoinMode] = None
+    lon: Optional[float] = None
+    lat: Optional[float] = None
+    active: Optional[bool] = None
+
+
+# Properties to receive on country creation
+class ParticipantCreate(ParticipantBase):
+    join_mode: JoinMode
+    lon: float
+    lat: float
+    active: bool = True
+
+
+# Properties to receive on update
+class ParticipantUpdate(ParticipantBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class ParticipantInDBBase(ParticipantBase):
+    id: int
+    join_mode: JoinMode
+    lon: float
+    lat: float
+    active: bool
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to return to client
+class Participant(ParticipantInDBBase):
+    pass
+
+
+# Properties properties stored in DB
+class ParticipantInDB(ParticipantInDBBase):
+    pass

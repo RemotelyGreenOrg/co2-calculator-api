cc249761aacd16738ba130aa5f2eabb19204d56c
benkrikler <4083697+benkrikler@users.noreply.github.com>
2022-02-04 13:02:27 +0100

    Refactor the main code for handling the websockets (#18)
    * Refactor the main code for events which handles websockets and
    participants
    
    * Switch back to pydantic and remove debugging messages
    
    * Remove common.py and put back original definitions
    
    * Update websocket endpoint to use refactored event-based approach
    
    * fix: Add static page serving back into app entrypoint
    
    * fix: Add websocket router to api
    
    Co-authored-by: Freddie Ruxton <ruxton.f@gmail.com>

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,13 +1,14 @@
 """Register API routers and modules"""
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import countries, cost_aggregator, health_check
+from app.api.api_v1.endpoints import countries, cost_aggregator, health_check, websocket
 from app.api.api_v1.modules import modules
 
 api_router = APIRouter()
 
 api_router.include_router(health_check.router, tags=["health_check"])
+api_router.include_router(websocket.router, tags=["websocket"])
+api_router.include_router(cost_aggregator.router, tags=["cost-aggregator"])
 api_router.include_router(countries.router, prefix="/countries", tags=["countries"])
-api_router.include_router(cost_aggregator.router)
 
 modules.register(api_router)
---
+++ app/schemas/event.py
@@ -0,0 +1,91 @@
+from typing import Optional, Union, Dict, Any
+from enum import Enum
+from starlette.websockets import WebSocket
+from pydantic import BaseModel
+from app.api.api_v1.endpoints.flight_calculator import GeoCoordinates
+
+
+class JoinMode(str, Enum):
+    online = "online"
+    in_person = "in_person"
+
+
+UID = str
+
+
+class ParticipantModel(BaseModel):
+    location: Union[GeoCoordinates, Dict[str, str]]
+    join_mode: JoinMode
+    websocket: Any
+    active: bool = True
+    uid: Optional[UID] = None
+
+
+class EventModelWebsocket:
+    def __init__(self, name: str, location: Union[GeoCoordinates, str]):
+        self.name = name
+        self.location = location
+        self.participants = {}
+
+    @property
+    def num_participants(self):
+        return len(self.participants)
+
+    @property
+    def participant_locations(self):
+        return [p.location for p in self.participants.values()]
+
+    async def add_participant(
+        self,
+        location: GeoCoordinates,
+        join_mode: JoinMode,
+        websocket: WebSocket,
+        # TODO: uid: str = None
+    ):
+        # TODO: needs to be provided externally to have any meaning
+        uid = str(self.num_participants)
+
+        parti = self.participants.get(uid, None)
+        if parti:
+            parti.active = True
+            parti.websocket = websocket
+        else:
+            parti = ParticipantModel(
+                location=location,
+                join_mode=JoinMode[join_mode],
+                websocket=websocket,
+                active=True,
+                uid=uid,
+            )
+            self.participants[uid] = parti
+
+        await self.update_sockets()
+
+    async def remove_participant(self, websocket: WebSocket):
+        # TODO: This should work of UID not websockets...
+        for participant in self.participants.values():
+            if participant.websocket == websocket:
+                participant.active = False
+                participant.websocket = None
+
+        await self.update_sockets()
+
+    def compute_footprint(self):
+        return 42 * self.num_participants
+
+    async def update_sockets(self):
+        results = self.compute_footprint()
+
+        for participant in self.participants.values():
+            if not participant.active:
+                continue
+            await participant.websocket.send_json(
+                {
+                    "event_name": self.name,
+                    "event_location": self.location,
+                    "participant_location": participant.location,
+                    "participant_locations": self.participant_locations,
+                    "event_participants": self.num_participants,
+                    "calculation": results,
+                }
+            )
